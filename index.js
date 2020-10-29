a58f5cea25a526eca9d0ae550617ca3f81e39b2a
Dan Gentile <dangentile@ymail.com>
2020-10-29 12:03:56 -0700

    updated server structure

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
+'use strict';
 // Required Dependencies 
-import express, { urlencoded, json, static } from "express";
-import logger from "morgan";
-import { connect } from "mongoose";
+const express = require('express')
+const logger = require('morgan')
+const mongoose = require('mongoose')
+
+// Imported Models 
+
 
 // Server 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(logger("dev"));
-app.use(urlencoded({ extended: true }));
-app.use(json());
-app.use(static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
 
-connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true });
-
-
-// Imported Models 
-
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true });
 
 // Imported Routes 
 
